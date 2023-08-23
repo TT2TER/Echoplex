9aa7b20a7635590fabf2cc26a15be3d82490de62
Alex.Sherlock <1246132896@qq.com>
2023-08-23 11:28:58 +0800

    aym update

--- db/DataDB.py
+++ db/DataDB.py
@@ -18,13 +18,17 @@ def create_table_user(con, table_name="user"):
     用户表：
     user_id : 用户编号
     user_name : 用户姓名
+    user_pwd : 用户密码
+    user_email : 邮箱
     user_image : 用户头像路径
     """
     cursor=con.cursor()
     try:
-        cursor.execute("CREATE TABLE"+ table_name+"("
+        cursor.execute("CREATE TABLE "+ table_name+" ("
                   "user_id INT PRIMARY KEY,"
                   "user_name text,"
+                  "user_pwd text,"
+                  "user_email text,"
                   "user_image text)")
         con.commit()
         print("table is created")
@@ -37,12 +41,11 @@ def create_table_group(con,table_name="group"):
     group_id : 群聊编号
     group_name : 群聊名
     group_leader_id : 群主编号
-    create_time : 建群时间
     group_image : 群头像
     """
     cursor=con.cursor()
     try:
-        cursor.execute("CREATE TABLE"+ table_name+"("
+        cursor.execute("CREATE TABLE "+ table_name+" ("
                   "group_id INT PRIMARY KEY,"
                   "group_name text,"
                   "group_leader_id INT UNIQUE,"
@@ -58,12 +61,14 @@ def create_table_user_friend(con,table_name="user-group"):
     用户好友关系表：        （主要用于好友界面）
     user_id : 用户编号
     friend_id : 好友编号
+    chat_id : 用户单聊天编号
     """
     cursor=con.cursor()
     try:
-        cursor.execute("CREATE TABLE"+ table_name+"("
-                  "user_id INT"
-                  "friend_id INT)")
+        cursor.execute("CREATE TABLE "+ table_name+" ("
+                  "user_id INT,"
+                  "friend_id INT,"
+                  "chat_id INT)")
         con.commit()
         print("table is created")
     except:
@@ -79,7 +84,7 @@ def create_table_user_chat(con,table_name="user-chat"):
     """
     cursor=con.cursor()
     try:
-        cursor.execute("CREATE TABLE"+ table_name+"("
+        cursor.execute("CREATE TABLE "+ table_name+" ("
                   "sender_id INT, "
                   "reciewer_id INT, "
                   "chat_time datatime,"
@@ -99,7 +104,7 @@ def create_table_group_chat(con,table_name="group-chat"):
     """
     cursor=con.cursor()
     try:
-        cursor.execute("CREATE TABLE"+ table_name+"("
+        cursor.execute("CREATE TABLE "+ table_name+" ("
                   "sender_id INTEGER ,"
                   "group_id INTEGER,"
                   "chat_time datatime,"
@@ -117,18 +122,101 @@ def create_table_group_member(con,table_name="group-member"):
     """
     cursor=con.cursor()
     try:
-        cursor.execute("CREATE TABLE"+ table_name+"("
+        cursor.execute("CREATE TABLE "+ table_name+" ("
                   "group_id INTEGER,"
                   "member_id INTEGER)")
         con.commit()
         print("table is created")
     except:
         print("table "+table_name+" is already exists")
-"""
+
+
+def insert_table_user(con,table_name,id,name,pwd,email,image):
+    cursor=con.cursor()
+    try:
+        sql="INSERT INTO "+table_name+" (user_id,user_name,user_pwd,user_email,user_image) VALUES(?,?,?,?,?)"
+        cursor.execute(sql,(id,name,pwd,email,image))
+        con.commit()
+        print("Successfully Insert")
+    except:
+        print("Insert Error")
+        con.rollback()
+
+def insert_table_group(con,table_name,id,name,image,leader_id):
+    cursor=con.cursor()
+    try:
+        sql="INSERT INTO "+table_name+" (group_id,group_name,group_leader_id,create_time,group_image) VALUES(?,?,?,?,?)"
+        cursor.execute(sql,(id,name,image,leader_id))
+        con.commit()
+        print("Successfully Insert")
+    except:
+        print("Insert Error")
+        con.rollback()
+
+def insert_table_user_friend(con,table_name,user_id,friend_id,chat_id):
+    cursor=con.cursor()
+    try:
+        sql="INSERT INTO "+table_name+"(user_id,friend_id,chat_id) VALUES(?,?,?,?,?)"
+        cursor.execute(sql,(user_id,friend_id,chat_id))
+        con.commit()
+        print("Successfully Insert")
+    except:
+        print("Insert Failed")
+        con.rollback()
+
+
+def update_table_user(con,table_name,id,index, value):
+    """
+    用户表更新            输入：
+    table_name : 操作的表名
+    id : 需要更新信息的用户的ID
+    index : 需要更新的选项（例如pwd和email）    注：前面需要加uer_
+    value : 更新的值
+    """
+    cursor=con.cursor()
+    try:
+        sql="UPDATE "+ table_name +" set "+index+"=? where user_id=?"
+        cursor.execute(sql,(value,id))
+        con.commit()
+        print("Successfully Update")
+    except:
+        print("Update Failed")
+        con.rollback()
+
+def select_table_user(con,table_name,**kwargs):
+    """
+    用户表查询：
+    table_name : 需要查询的表
+    **kwargs : 查询条件（例如：user_name="'xa'"和user_id=1002）
+
+    返回 一个用户所有信息
+    """
+    cursor=con.cursor()
+    try:
+        sql="SELECT * FROM "+table_name+" WHERE "
+        flag=0
+        for key,value in kwargs.items():
+            if flag==1:
+                sql=sql+" AND "
+            print(key,value)
+            sql=sql+key+"="+str(value)
+            flag=1
+        print(sql)
+        cursor.execute(sql)
+        ret=cursor.fetchone()
+        return ret
+    except:
+        print("Select Failed")
+        return None
+#def select_table_user(con,)
+'''
 con=sql_connection()
 table_name="user"
 create_table_user(con,table_name)
-
-
+#insert_table_user(con,table_name,id=1002,name="xa",pwd="123456",email="12@qq.com",image="path/to/image")
+update_table_user(con,table_name=table_name,id=1002,index='user_email',value='1234@qq.com')
+a=select_table_user(con,table_name,user_name="'xa'",user_id=1002)
+print(a)
 con.close()
-"""
+'''
+

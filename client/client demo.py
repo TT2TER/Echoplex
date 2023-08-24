0460363733aec3263e6e745bfe11c2ff3fe463e0
FluppyFR <flyingrabbit0527@outlook.com>
2023-08-24 15:47:53 +0800

    Update client demo.py

--- client/client demo.py
+++ client/client demo.py
@@ -1,40 +1,78 @@
 import socket
-import json
 import threading
+import json
 
 
-def receive_messages(client_socket):
+def receive_messages():
     while True:
         try:
-            data = client_socket.recv(1024)
-            if data:
-                received_data = json.loads(data.decode('utf-8'))
-                print("收到消息:", received_data)
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            handle_message(data)
         except:
             break
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('127.0.0.1', 13579)
-client_socket.connect(server_address)
+def handle_message(data):
+    try:
+        message = json.loads(data)
+        msg_type = message["type"]
+        content = message["content"]
 
-receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+        if msg_type == "new_message":
+            sender = content["sender"]
+            msg = content["msg"]
+            print(f"New message from {sender}: {msg}")
+
+        # Add other message types handling here
+
+    except json.JSONDecodeError:
+        pass
+
+
+def send_messages():
+    while True:
+        msg_type = input("Enter message type (friend_chat/group_chat): ")
+        if msg_type not in ["friend_chat", "group_chat"]:
+            print("Invalid message type")
+            continue
+
+        sender = input("Enter your username: ")
+        msg = input("Enter message: ")
+
+        if msg_type == "friend_chat":
+            receiver = input("Enter receiver's username: ")
+            message = {
+                "type": msg_type,
+                "content": {
+                    "sender": sender,
+                    "receiver": receiver,
+                    "msg": msg
+                }
+            }
+        elif msg_type == "group_chat":
+            group_id = input("Enter group ID: ")
+            message = {
+                "type": msg_type,
+                "content": {
+                    "sender": sender,
+                    "group_id": group_id,
+                    "msg": msg
+                }
+            }
+
+        client_socket.send(json.dumps(message).encode('utf-8'))
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('127.0.0.1', 12345))
+
+receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
 
-while True:
-    # group_id = input("输入群聊ID: ")
-    # sender = input("输入您的用户名: ")
-    content = input("输入消息内容: ")
-    # visible_to = input("输入可见的用户名列表，以逗号分隔: ").split(',')
+send_thread = threading.Thread(target=send_messages)
+send_thread.start()
 
-    message = {
-        "type": "register",
-        # "timestamp": "2023-08-22T12:34:56",
-        # "sender": sender,
-        # "group_id": group_id,
-        "content": content,
-        # "visible_to": visible_to
-    }
-
-    json_data = json.dumps(message).encode('utf-8')
-    client_socket.sendall(json_data)
+send_thread.join()
+client_socket.close()

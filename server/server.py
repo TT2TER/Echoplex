3f13007c9b9c60525628050cb38e9f98c60b1a9a
FluppyFR <flyingrabbit0527@outlook.com>
2023-08-24 15:46:41 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -4,47 +4,55 @@ import json
 from user_register import user_register
 from user_login import user_login
 from db.DataDB import *
+from db.table_user import *
 import sys
 from user_chat import user_chat
+from global_data import online_clients
 
-clients = []
 
 def handle_client(socket, address):
-    # bufsize 指定要接收的最大数据量
     try:
         database = sql_connection()
         print("数据库打开成功，好耶")
     except:
         print("handle_client获取数据库连接失败，小艾的锅")
-    try:
-        data = socket.recv(1024)
-        received_data = json.loads(data.decode('utf-8'))
-        print("收到客户端消息：", received_data)
-    except Exception as e:
-        print("没收到客户端消息或者收消息过程中寄了，自己看报错吧：" + str(e))
-    try:
-        print("处理ing……")
-        message_handlers = {
-            'user_register': user_register,
-            'user_login': user_login,
-            'user_chat':user_chat
-        }
-        handler = message_handlers.get(received_data['type'])
-        if handler:
-            print("handler为" + getattr(handler, "__name__", "unknown_function"))
-            succ = handler(received_data, socket, address, database)
-            print("处理结果：" + str(succ))
-        else:
-            print("收到了服务器不认识的消息类型欸")
-    except Exception as e:
-        print(str(address) + " 连接异常，准备断开: " + str(e))
-    finally:
+        return
+    while True:
         try:
-            socket.close()
-            print("服务器完工，顺利下班oVo")
+            # bufsize 指定要接收的最大数据量
+            data = socket.recv(10240)
+            received_data = json.loads(data.decode('utf-8'))
+            print("收到客户端消息：", received_data)
         except Exception as e:
-            print(str(address) + "连接关闭异常:" + str(e))
-
+            print("客户端已经下线或者收消息过程中寄了，自己看报错吧：" + str(e))
+            break
+        try:
+            print("处理ing……")
+            message_handlers = {
+                'user_register': user_register,
+                'user_login': user_login,
+                'user_chat': user_chat
+            }
+            handler = message_handlers.get(received_data['type'])
+            if handler:
+                print("handler为" + getattr(handler, "__name__", "unknown_function"))
+                succ = handler(received_data, socket, address, database)
+                print("处理结果：" + str(succ))
+            else:
+                print("收到了服务器不认识的消息类型欸")
+                break
+        except Exception as e:
+            print(str(address) + " 连接异常，准备断开: " + str(e))
+            break
+        finally:
+            print("服务器完工，等待下一个请求oVo")
+    try:
+        # online_clients.pop(socket,None)
+        # TODO
+        # 维护online_clients字典
+        pass
+    except:
+        pass
 
 def init_server(database):
     create_table_user(database, )
@@ -70,16 +78,13 @@ if __name__ == "__main__":
         server_socket.bind(server_address)
 
         # TCP 监听
-        # backlog 指定在拒绝连接之前，操作系统可以挂起的最大连接数量�?
-        # 该值至少为 1
-        # 大部分应用程序设5就可以了
-        backlog = 5
+        # backlog 指定在拒绝连接之前，操作系统可以挂起的最大连接数量s
+        backlog = 20
         server_socket.listen(backlog)
         print("服务器等待连接？O.o")
         while True:
             new_socket, client_address = server_socket.accept()
-            clients.append((new_socket, client_address))
-            print("服务器连接上了客户端"+client_address+"，准备干活！")
+            print("服务器连接上了客户端" + str(client_address) + "，准备干活！")
             client_handler = threading.Thread(target=handle_client, args=(new_socket, client_address))
             client_handler.start()
     except Exception as e:

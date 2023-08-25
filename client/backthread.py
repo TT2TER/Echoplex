c0829bd8a0d79d39b2d9f9686f35263fa2aee0d8
SanukiUra <theophilus03@foxmail.com>
2023-08-25 17:32:04 +0800

    fix client监听server 代码重构

--- client/backthread.py
+++ client/backthread.py
@@ -1,26 +1,25 @@
-from PySide2.QtCore import QThread ,  Signal,  QDateTime 
-from PySide2.QtWidgets import QApplication,  QDialog,  QLineEdit
+from PySide2.QtCore import QThread, Signal, QDateTime
+from PySide2.QtWidgets import QApplication, QDialog, QLineEdit
+import json
+from lib.public import shared_module
+
 
 class BackendThread(QThread):
     # 通过类成员对象定义信号对象  
-	nofify = Signal(dict)
-	
-    # 处理要做的业务逻辑
-	def run(self):
-		while True:
+    nofify = Signal(dict)
 
-			#以下是emit样例
-			your_message= {
-    			'first_name': 'John',
-    			'last_name': 'Doe',
-    			'age': 30,
-    			'city': 'New York',
-    			'email': 'john.doe@example.com'
-			}
-			#发射信号样例,发射信号会传到主进程，被shared_module.client.back_massage_handler处理掉
-			self.nofify.emit(your_message)
-			
+    # 处理要做的业务逻辑
+    def run(self):
+        while True:
+            try:
+                data = shared_module.client.client_socket.recv(10240)
+                received_data = json.loads(data.decode('utf-8'))
+                print("收到服务端消息：", received_data)
+                self.nofify.emit(received_data)
+            except Exception as e:
+                print("服务端寄了或者收消息寄了：" + str(e))
+                break
 
 
 if __name__ == '__main__':
-	print("调试线程\n")
+    print("调试线程\n")

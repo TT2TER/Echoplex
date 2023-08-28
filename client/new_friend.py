939ccb1640105ac2935692660be2e936baaba028
Mayu-ubuntu <2270610334@qq.com>
2023-08-28 09:36:43 +0800

    mayu 優化加好友

--- client/new_friend.py
+++ client/new_friend.py
@@ -2,16 +2,34 @@ from PySide2.QtWidgets import QWidget, QListWidgetItem
 from ui.new_friends_ui import Ui_new_friends
 from lib.public import shared_module
 from new_friend_bar import New_friend_bar
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QMouseEvent
 
 
 class New_friends(QWidget):
     def __init__(self):
         # 继承父类
         super().__init__()
+        # Set window flags to customize the window behavior
+        self.setWindowFlags(Qt.FramelessWindowHint)
         self.ui = Ui_new_friends()
         self.ui.setupUi(self)
+        
         self.ui.back_butt.clicked.connect(self.back)
         self.item = []
+
+    def mousePressEvent(self, event: QMouseEvent):
+        if event.button() == Qt.LeftButton:
+            self.dragging = True
+            self.offset = event.globalPos() - self.pos()
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        if self.dragging:
+            self.move(event.globalPos() - self.offset)
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        if event.button() == Qt.LeftButton:
+            self.dragging = False
 
     def back(self):
         self.ui.new_friends_box.clear()

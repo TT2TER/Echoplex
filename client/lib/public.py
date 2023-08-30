2d3363fec233923f0b3980afc22048173dd87695
FluppyFR <flyingrabbit0527@outlook.com>
2023-08-31 04:14:27 +0800

    Update public.py

--- client/lib/public.py
+++ client/lib/public.py
@@ -1,24 +1,24 @@
 class shared_module:
-    #以下是整个程序
-    app=None
-    #以下是ui的实例化对象
-    login_page=None
-    main_page=None
-    reg_page=None
-    setip_page=None
-    add_friend=None
-    new_friends=None
-    progress_bar=None
-    new_group=None
-    manage_group=None
-    video_page=None
-    #以下是功能实例化对象
-    client=None 
-    #以下是线程实例化对象
+    # 以下是整个程序
+    app = None
+    # 以下是ui的实例化对象
+    login_page = None
+    main_page = None
+    reg_page = None
+    setip_page = None
+    add_friend = None
+    new_friends = None
+    progress_bar = None
+    new_group = None
+    manage_group = None
+    video_page = None
+    # 以下是功能实例化对象
+    client = None
+    # 以下是线程实例化对象
     file_thread = None
-    listen_thread=None
+    listen_thread = None
     video_thread = None
-
-    #以下是调试参数
-    full_fuction=True
+    # 以下是调试参数
+    full_fuction = True
     FileSendThread_signal = None
+    token_enabled = False
